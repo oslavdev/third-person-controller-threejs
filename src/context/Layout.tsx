417268b9f118510b686d9d89c0e38f5c0c32c2a7
Jaroslaw <yaroslav@xuver-services.com>
2020-12-17 14:40:01 +0200

    create pages

---
+++ src/context/Layout.tsx
@@ -0,0 +1,10 @@
+import React, {ReactNode} from 'react'
+
+
+export default function Layout(children:ReactNode) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
---
+++ src/pages/ToolBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ToolBox() {
+  return (
+    <div>
+      ToolBox
+    </div>
+  )
+}
